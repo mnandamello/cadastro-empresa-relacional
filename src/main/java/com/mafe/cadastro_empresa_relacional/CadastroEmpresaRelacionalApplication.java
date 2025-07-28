700c5eb6ca2d7fe9a0255de4d869c9485130dd0b
mnandamello <mfernanda.nintendo@gmail.com>
2025-07-27 22:13:11 -0300

    Initial commit

---
+++ src/main/java/com/mafe/cadastro_empresa_relacional/CadastroEmpresaRelacionalApplication.java
@@ -0,0 +1,13 @@
+package com.mafe.cadastro_empresa_relacional;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEmpresaRelacionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEmpresaRelacionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mafe/cadastro_empresa_relacional/CadastroEmpresaRelacionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mafe.cadastro_empresa_relacional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEmpresaRelacionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
