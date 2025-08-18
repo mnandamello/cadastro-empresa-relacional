765a5c4bd29a2f50d46cd5213616110e4ad5ed1a
mnandamello <mfernanda.nintendo@gmail.com>
2025-08-17 22:29:19 -0300

    feat: changing the findByEmail method return

--- src/main/java/com/mafe/cadastroempresarelacional/infraestructure/repositorys/UserRepository.java
+++ src/main/java/com/mafe/cadastroempresarelacional/infraestructure/repositorys/UserRepository.java
@@ -4,7 +4,8 @@ import com.mafe.cadastroempresarelacional.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    List<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
