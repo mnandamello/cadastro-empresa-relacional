6a3a755732bc94acfcf34b055e50e2d4a59c5aa6
mnandamello <mfernanda.nintendo@gmail.com>
2025-08-24 20:24:43 -0300

    feat: creating company class and enum

---
+++ src/main/java/com/mafe/cadastroempresarelacional/domain/Company.java
@@ -0,0 +1,46 @@
+package com.mafe.cadastroempresarelacional.domain;
+
+import com.mafe.cadastroempresarelacional.domain.enums.CompanySituation;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+@Table(name = "companies")
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Company {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cnpj;
+
+    @Column(name = "business_name")
+    private String businessName;
+
+    @Column(name = "trade_name")
+    private String tradeName;
+
+    private String phone;
+
+    @Column(name = "contact_email")
+    private String contactEmail;
+
+    @Column(name = "foundation_data")
+    private Date foundationData;
+
+    @Column(name = "company_situation")
+    private CompanySituation companySituation;
+
+    @OneToMany(mappedBy = "company", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Adress> adresses;
+}
---
+++ src/main/java/com/mafe/cadastroempresarelacional/domain/enums/CompanySituation.java
@@ -0,0 +1,6 @@
+package com.mafe.cadastroempresarelacional.domain.enums;
+
+public enum CompanySituation {
+    ACTIVE,
+    INACTIVE
+}
