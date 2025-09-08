9d58839080f8ab0538f5ae231aeb3cfc05c9b2ca
mnandamello <mfernanda.nintendo@gmail.com>
2025-09-08 18:14:47 -0300

    feat: add new method in CompanyController

--- src/main/java/com/mafe/cadastroempresarelacional/interfaces/controllers/CompanyController.java
+++ src/main/java/com/mafe/cadastroempresarelacional/interfaces/controllers/CompanyController.java
@@ -1,16 +1,16 @@
 package com.mafe.cadastroempresarelacional.interfaces.controllers;
 
 import com.mafe.cadastroempresarelacional.application.CompanyService;
+import com.mafe.cadastroempresarelacional.domain.Company;
+import com.mafe.cadastroempresarelacional.domain.User;
 import com.mafe.cadastroempresarelacional.interfaces.dto.request.CreateCompanyRequest;
 import com.mafe.cadastroempresarelacional.interfaces.dto.response.ApiResponse;
+import com.mafe.cadastroempresarelacional.interfaces.dto.response.CompanyResponse;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/company")
@@ -28,5 +28,13 @@ public class CompanyController {
         ApiResponse apiResponse = companyService.createCompany(request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(apiResponse);
+    }
+
+    @PreAuthorize("hasAnyAuthority('ADMIN', 'BASIC')")
+    @GetMapping("/api/v1/companies/{cnpj}")
+    public ResponseEntity<CompanyResponse> getCompanyByCnpj(@PathVariable String cnpj){
+        CompanyResponse company = companyService.getByCnpj(cnpj);
+
+        return ResponseEntity.status(HttpStatus.OK).body(company);
     }
 }
